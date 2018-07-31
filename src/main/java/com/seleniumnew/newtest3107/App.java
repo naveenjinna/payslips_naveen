e7f3aaa3c1808766f741e19e29eb2a2c5428a57d
bharath parshi <bharath.p@easywaytech.com>
2018-07-31 17:38:40 +0530

    1st commit

---
+++ src/main/java/com/seleniumnew/newtest3107/App.java
@@ -0,0 +1,13 @@
+package com.seleniumnew.newtest3107;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
